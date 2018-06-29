0d59e42dbcf69ee5570baa343e6f2556f5bb5e7a
SwathiReddy-p <quadrantlenora@outlook.com>
2018-06-28 22:02:58 -0700

    Declaring function name and calling the function to get sshdetails

--- DeployAzureVM.ps1
+++ DeployAzureVM.ps1
@@ -1,5 +1,6 @@
 # Declaring the parameter that to give at run time 
 param(
+	[Parameter(Mandatory=$True)]
 	[string]
 	$subscriptionId,
 
@@ -39,8 +40,8 @@ else
 # Deploying VM and checking whether it is succeeded or not
 $name="MyUbuntuVM"
 Write-Host "Creating and Deploying the VM"
-$vm = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "Templates\azuredeploy.json" -TemplateParameterFile "Templates\azuredeploy.parameters.json"
-if ($vm.ProvisioningState -eq "Succeeded")
+$Deployvm = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "Templates\azuredeploy.json" -TemplateParameterFile "Templates\azuredeploy.parameters.json"
+if ($Deployvm.ProvisioningState -eq "Succeeded")
 {
     $MaxTimeOut=300
     $i=0
@@ -48,9 +49,8 @@ if ($vm.ProvisioningState -eq "Succeeded")
     {
         $vmDetail=Get-AzureRmVM -ResourceGroupName $resourceGroupName -Name $name  -Status
         if($vmDetail.Statuses[0].DisplayStatus -eq  "Provisioning succeeded")
-        {
-			$vm			
-            Write-Host "Deployment completed succesfully"
+        {	
+            Write-Host "Deployment completed succesfully so VM has created "
             break
         }
         else
@@ -63,27 +63,31 @@ if ($vm.ProvisioningState -eq "Succeeded")
     {
         Write-Host "Deployment failed"
     } 
+    GetIPAddress
 }
 
 #To get the Username, Password and PublicIp Address of the VM that created
-Write-Host "Getting the IpAddress of the VM"
-if ($vm.ProvisioningState -eq "Succeeded")
+Function GetIPAddress
 {
-    $var=Get-Content "Templates\azuredeploy.parameters.json"| ConvertFrom-Json 
-    $Username=$var.parameters.adminUsername.value
-    Write-Host " UserName of the VM is "$Username
+	Write-Host "Getting the IpAddress of the VM"
+	if ($Deployvm.ProvisioningState -eq "Succeeded")
+	{
+		$var=Get-Content "Templates\azuredeploy.parameters.json"| ConvertFrom-Json 
+		$Username=$var.parameters.adminUsername.value
+		Write-Host " UserName of the VM is "$Username
 
-    $Password=$var.parameters.adminPassword.value 
-    $SPassword= ConvertTo-SecureString $Password -AsPlainText -Force
-    Write-Host " The password is " $SPassword
+		$Password=$var.parameters.adminPassword.value 
+		$SPassword= ConvertTo-SecureString $Password -AsPlainText -Force
+		Write-Host " The password is " $SPassword
 
-    $IPAddress=Get-AzureRmPublicIpAddress -ResourceGroupName $resourceGroupName  -Name MyPublicIp | Select-Object  IpAddress
-    Write-Host "IPAddress is" $IPAddress.IpAddress
+		$IPAddress=Get-AzureRmPublicIpAddress -ResourceGroupName $resourceGroupName  -Name MyPublicIp | Select-Object  IpAddress
+		Write-Host "IPAddress is" $IPAddress.IpAddress
 
-    $sshdetails= 'ssh ' + $Username + '@' + $IPAddress.IpAddress
-    $sshdetails
-}
-else
-{
-    Write-Host "VM does not exit"
+		$sshdetails= 'ssh ' + $Username + '@' + $IPAddress.IpAddress
+		$sshdetails
+	}
+	else
+	{
+		Write-Host "VM does not exit"
+	}
 }
