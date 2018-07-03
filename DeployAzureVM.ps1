2f002341f60250c55f74895c34a4f8686967d653
SwathiReddy-p <quadrantlenora@outlook.com>
2018-07-03 02:10:42 -0700

    Moved VM creation code into a function

--- DeployAzureVM.ps1
+++ DeployAzureVM.ps1
@@ -1,14 +1,13 @@
 
-
 # Declaring the parameter that to give at run time 
 param (
-	[string] $subscriptionId = "YourSubscription",
+	[string] $subscriptionId = "Your subscription",
 	[string] $resourceGroupName = "MyRG",
 	[string] $location = "eastus",
+    [string] $TemplateFile = "Templates\azuredeploy.json",
+    [string] $TemplateParameterFile = "Templates\azuredeploy.parameters.json",
 	[switch] $Debug = $false
 )
-
-exit
 
 . .\libs\sshUtils.ps1
 # Sigining in to the portal
@@ -18,59 +17,87 @@ Login-AzureRmAccount
 # Getting the subsciptions from the portal
 Get-AzureRmSubscription -SubscriptionId $subscriptionId
 
-#select subscription
-Write-Host "Select the subscriptions from"
+# select subscription
+Write-Host  -ForegroundColor Yellow "Set the subscriptions";
 Set-AzureRmContext -Subscription $subscriptionId
 
+# Deploying VM and checking whether it is succeeded or not
+Function DeploySingleLinuxVM()
+{
+	$name="MyUbuntuVM"
+	$VMdeploy = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $TemplateFile  -TemplateParameterFile $TemplateParameterFile
+	if ($VMdeploy.ProvisioningState -eq "Succeeded")
+	{
+		$MaxTimeOut=300
+		$i=0
+		while($MaxTimeOut -gt $i)
+		{
+			$vmDetail=Get-AzureRmVM -ResourceGroupName $resourceGroupName -Name $name  -Status
+			if($vmDetail.Statuses[0].DisplayStatus -eq  "Provisioning succeeded")
+			{
+				Write-Host "Created VM successfully"
+				break
+			}
+			else
+			{
+				Write-Host -NoNewline "." #print a . without newline
+			}
+			$i=$i+1
+		}
+		if ($MaxTimeOut -eq $i)
+		{
+			Write-Host  -ForegroundColor Yellow "Deployment failed"
+		} 
+    
+	}
+	#calling the function to get the sshdetails
+	Getsshdetails
+}
+
+#Function get the Username, Password and PublicIp Address of the VM that created
+Function Getsshdetails()
+{
+	Write-Host  -ForegroundColor Yellow "Getting the Username and IpAddress of the VM"
+    if ($VMdeploy.ProvisioningState -eq "Succeeded")
+    {
+        $var=Get-Content $TemplateParameterFile | ConvertFrom-Json 
+        $Username=$var.parameters.adminUsername.value
+        Write-Host " UserName of the VM is : "$Username
+
+        $Password=$var.parameters.adminPassword.value 
+        Write-Host " Password is : " $Password
+
+        $IPAddress=Get-AzureRmPublicIpAddress -ResourceGroupName $resourceGroupName  -Name MyPublicIp | Select-Object  IpAddress
+        Write-Host  " IPAddress of VM is :" $IPAddress.IpAddress
+
+        $sshdetails= 'ssh ' + $Username + '@' + $IPAddress.IpAddress
+        $sshdetails  
+    }
+    else
+    {
+        Write-Host "VM does not exit"
+    }
+}
+
 # Create or using existing resourceGroup 
-Write-Host "Verifying ResourceGroupName exit or not: '$resourceGroupName'"
+Write-Host  -ForegroundColor Yellow "Verifying the ResourceGroup exist or not: '$resourceGroupName'"
 $resourcegroup = Get-AzureRmResourceGroup -Name $resourceGroupName -Location $location -erroraction silentlycontinue
 if(!$resourcegroup)
 {
-	Write-Host "Creating ResourceGroup: '$resourceGroupName'"
+	Write-Host  -ForegroundColor Yellow "Creating ResourceGroup: '$resourceGroupName'"
 	New-AzureRmResourceGroup -Name $resourceGroupName -Location $location 
 }
 else
 {
-	Write-Host "Resourcegroup: '$resourceGroupName' already exists"
+	Write-Host  -ForegroundColor Yellow "ResourceGroupName is alresy exisited"
 }
 
-# Deploying VM and checking whether it is succeeded or not
-$name="MyUbuntuVM"
-Write-Host "Creating and Deploying the VM"
-$RGdeployment = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "Templates\azuredeploy.json" -TemplateParameterFile "Templates\azuredeploy.parameters.json"
-if ($RGdeployment.ProvisioningState -eq "Succeeded")
-{
-    $MaxTimeOut=300
-    $i=0
-    while($MaxTimeOut -gt $i)
-    {
-        $vmDetail=Get-AzureRmVM -ResourceGroupName $resourceGroupName -Name $name  -Status
-        if($vmDetail.Statuses[0].DisplayStatus -eq  "Provisioning succeeded")
-        {
-			$RGdeployment			#Displaying the ssh details of the VM
-            Write-Host "Deployment completed succesfully"
-            break
-        }
-        else
-        {
-            Write-Host -NoNewline "." 	#print a . without newline
-        }
-        $i=$i+1
-    }
-    if ($MaxTimeOut -eq $i)
-    {
-        Write-Host "Deployment failed"
-    } 
-}
+Write-Host -ForegroundColor Yellow "Creating and Deploying the VM"
 
-#To get the PublicIp Address of the VM that created
-Write-Host "Getting the IpAddress of the VM"
-if ($RGdeployment.ProvisioningState -eq "Succeeded")
-{
-    Get-AzureRmPublicIpAddress -ResourceGroupName $resourceGroupName  -Name MyPublicIp | Select-Object ResourceGroupName, Name, IpAddress
-}
-else
-{
-    Write-Host "IpAddress not found"
-}
+#Calling the function to create and deploy the VM
+DeploySingleLinuxVM
+
+
+
+
+ 
