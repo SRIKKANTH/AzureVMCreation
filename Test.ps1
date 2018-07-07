df4a1b2398013572dc1ac8059c954ebfffedf0cf
Srikanth Myakam <v-srm@microsoft.com>
2018-07-07 04:03:26 -0700

    Added Test.ps1

---
+++ Test.ps1
@@ -0,0 +1,24 @@
+<#
+Use this to test your changes without actually deploying the VM
+#>
+$Debug = $false
+$TestName = "DeploySingleVM"
+$TemplateType = "1VM_1ManagedDisk" 
+
+. .\libs\AzureLibs.ps1
+. .\libs\sshUtils.ps1
+
+
+$RGDetails = GetRGDetails $TestName
+$VMDetails = New-Object -TypeName PSObject -Property $VMproperties
+$VMDetails.IP = "10.135.16.212"
+$VMDetails.Port = 22;
+$VMDetails.VMName = "UnDeclared"
+$VMDetails.UserName="naga"
+$VMDetails.PassWord="SamplePassword"
+
+#WaitTillMachineBoots $VMDetails
+UploadFiles -VMDetails $VMDetails -RGDetails  $RGDetails 
+
+
+Exit
